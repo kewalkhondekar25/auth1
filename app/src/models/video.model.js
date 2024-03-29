e081146be6ab1b4f67366b00f835cbd2a497e95e
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-29 23:32:27 +0530

    feat: mongoDB aggregation pipeline added

--- app/src/models/video.model.js
+++ app/src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
 const videoSchema = new mongoose.Schema({
     videoFile: {
@@ -35,4 +36,5 @@ const videoSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
+videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = mongoose.model("Video", videoSchema);
