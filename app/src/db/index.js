ccd725593b1805a60bea11d05668896daa099e46
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-29 18:51:44 +0530

    feat: mongdb connected

--- app/src/db/index.js
+++ app/src/db/index.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants"
+import {DB_NAME} from "../constants.js"
 
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MONGODB CONNECTED!!! \n DB HOST: ${connectionInstance.connection.host}`);
+        console.log(`MONGODB CONNECTED!!! \n DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.error("MONGODB CONNECTION ERROR: ", error);
+        console.error("MONGODB CONNECTION FAILED: ", error);
         process.exit(1);
     }
 };
