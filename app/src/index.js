6b1376312ef25b3d50edacbaa788c6b08d3ce195
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-29 19:19:59 +0530

    feat: added cookie-parser & cors added

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: "./env"
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`SERVER IS LISTENING ON PORT: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.error("MONGODB CONNECTION FAILED: ", error);
+});
