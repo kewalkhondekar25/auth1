0adb8b99190949ae28975b447d8b5399a1b63a15
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-30 00:53:00 +0530

    feat: mongoose pre, methods & jwt tokens added

--- app/src/models/user.model.js
+++ app/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchame = new mongoose.Schema(
     {
@@ -46,4 +48,39 @@ const userSchame = new mongoose.Schema(
     }, 
     {timestamps: true});
 
+userSchame.pre("save", async function(next){
+    if(!this.isModified("password")) {
+        return next();
+    };
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+userSchame.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+userSchame.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+};
+userSchame.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+        _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
 export const User = mongoose.model("User", userSchame);
