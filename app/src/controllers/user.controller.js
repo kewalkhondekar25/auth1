91155d10c12a67cb469be8ce86abd17277ca0b0c
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-30 17:54:30 +0530

    feat: ZOD implementation added

--- app/src/controllers/user.controller.js
+++ app/src/controllers/user.controller.js
@@ -1,9 +1,25 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import zod from "zod";
+
+const registerValidation = zod.object({
+    userName: zod.string().min(1),
+    email: zod.string().min(1).email(),
+    password: zod.string().min(3),
+    fullName: zod.string().min(1),
+})
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "this is response from register user"
-    });
+    const registerUserInput = req.body;
+    const response = registerValidation.safeParse(registerUserInput);
+    if(!response.success){
+        res.status(400).json({
+            message: response.error
+        })
+    }else{
+        res.status(200).json({
+            response
+        })
+    }
 });
 
 export default registerUser
