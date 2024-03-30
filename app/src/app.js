e502ca91be316ddb403f4ad1958b7616c7724dd7
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-30 15:14:12 +0530

    feat: user route & controller implemented

--- app/src/app.js
+++ app/src/app.js
@@ -18,6 +18,10 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//import routes
+import userRouter from "./routes/user.route.js";
 
+//declare routes
+app.use("/api/v1/users", userRouter);
 
 export default app;
