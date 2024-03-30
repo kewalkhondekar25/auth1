49ffd1900957f7431f62e4a3d0e47d3bd84e139c
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-30 19:31:07 +0530

    fix: async bcrypt await added

--- app/src/models/user.model.js
+++ app/src/models/user.model.js
@@ -52,7 +52,7 @@ userSchame.pre("save", async function(next){
     if(!this.isModified("password")) {
         return next();
     };
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 userSchame.methods.isPasswordCorrect = async function(password){
