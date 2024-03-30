4818cfb7b220e5180b3cbc44906fd2042cbca775
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-30 23:58:10 +0530

    fix: user controller bug fixed

--- app/src/controllers/user.controller.js
+++ app/src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import zod from "zod";
 import { User } from "../models/user.model.js";
 
 const registerUserValidation = zod.object({
-    userName: zod.string().min(1),
+    username: zod.string().min(1),
     email: zod.string().min(1).email(),
     password: zod.string().min(3),
     fullName: zod.string().min(1),
@@ -12,28 +12,29 @@ const registerUserValidation = zod.object({
 const registerUser = asyncHandler( async (req, res) => {
     const registerUserInput = req.body;
     const validResponse = registerUserValidation.safeParse(registerUserInput);
-    //existing user
-    const existedUser = User.findOne({
-        $or: [{userName}, {email}]
-    })
-    if(existedUser){
-        return res.status(409).json({
-            message: "user already exist"
-        })
-    };
-    //images
-    // const avatarLocalPath = req.files?.avatar[0]?.path;
-    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
+    const payload = validResponse.data;
+    const {username, email} = payload;
+    
+    //validation
     if(!validResponse.success){
-        res.status(400).json({
+        return res.status(400).json({
             message: validResponse.error
         })
-    }else{
-        res.status(200).json({
-            validResponse
-        })
-    }
+    };
+    //already exist user
+    const existUser = await User.findOne({
+        $or: [{username}, {email}]
+    });
+    if(existUser){
+        return res.status(409).json({
+            message: "User with username or email already exists"
+        });
+    };
+    //creating entry
+    await User.create(payload);
+    return res.status(200).json({
+        message: "User created successfully"
+    });
 });
 
 export default registerUser
